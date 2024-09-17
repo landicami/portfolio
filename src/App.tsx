15530b670996c66e32d24405b447384c97101d4f
camillalandin <camilla.landin@mtm.se>
2024-09-17 13:37:38 +0200

    Routes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import Container from "react-bootstrap/Container"
 import HomePage from './pages/HomePage'
+import Navigation from './components/Navigation'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
 
   return (
     <Container>
-    <Routes>
-      <Route path="/" element={<HomePage/>} />
-    </Routes>
+      <Navigation />
+    
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="*" element={<NotFoundPage />}/>
+      </Routes>
      
     </Container>
   )
