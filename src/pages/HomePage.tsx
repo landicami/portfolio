c269c322c06f5aa937e004efcc03001161bdd094
camillalandin <camilla.landin@mtm.se>
2024-09-17 13:38:01 +0200

    HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Button from "react-bootstrap/Button";
+import AboutMe from '../components/AboutMe'
+import Skills from '../components/Skills';
 
 const HomePage = () => {
-  return (
-    <div><h1>Welcome to my portfolio</h1></div>
+    const email = "landin.camilla8@gmail.com";
+    const copyEmail = () => {
+        navigator.clipboard.writeText(email).then(() => {
+            alert("Email copied");
+        })
+        .catch(error => {
+            console.log("Failed to copy email", error)
+        } 
+    )};
+
+  return (<>
+  <div>
+    <h1 className='mt-5'>Welcome to my portfolio</h1>
+    <h2>Camilla Landin</h2>
+    <h3>Front end developer</h3>
+    <Button variant="secondary" onClick={copyEmail}>✉️ Email me</Button>
+    <hr/>
+    </div>
+    <AboutMe/>
+    <hr/>
+
+    <Skills/>
+    </>
   )
 }
 
