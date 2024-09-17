accae7f97c74b573c7043029230396b8b1f107cb
camillalandin <camilla.landin@mtm.se>
2024-09-17 15:40:15 +0200

    Skillspage is huge

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import Container from "react-bootstrap/Container"
 import HomePage from './pages/HomePage'
 import Navigation from './components/Navigation'
 import NotFoundPage from './pages/NotFoundPage'
+import SkillsPage from './pages/SkillsPage'
 
 function App() {
 
   return (
     <Container>
       <Navigation />
-    
+
       <Routes>
         <Route path="/" element={<HomePage/>} />
+		<Route path="/skills" element={<SkillsPage/>} />
         <Route path="*" element={<NotFoundPage />}/>
       </Routes>
-     
+
     </Container>
   )
 }
---
+++ src/pages/SkillsPage.tsx
@@ -0,0 +1,18 @@
+import Skills from '../components/Skills'
+import TopSection from '../components/TopSection'
+
+const SkillsPage = () => {
+	return (
+	<>
+		<div>
+		<TopSection />
+		</div>
+		<hr/>
+		<Skills/>
+
+	</>
+
+)
+}
+
+export default SkillsPage
